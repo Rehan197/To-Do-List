52e585732044fe69ccb02e2a6d0eb6ad52f750fd
rehan.raees <rehan.raees1@gmail.com>
2023-10-15 19:20:55 +0500

    Project Complete

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in your todo? "
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo? ",
+            default: false
+        }
+    ]);
+    const { TODO, addmore } = answers;
+    console.log(answers);
+    loop = addmore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your TOdo List:");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todos found");
+}
